b4e16b0017e1c8ec6bcdf6069af0fdf1b1a9fbeb
Md Ibrahim <mdrafi15429@gmail.com>
2022-06-01 03:01:55 +0600

    navber added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Nabver from './Pages/Shared/Nabver';
-
+import Home from './Pages/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Longin from './Pages/Auth/Longin';
 function App() {
   return (
-    <div>
-      <Nabver></Nabver>
-    </div>
+    <>
+    <Nabver>
+    </Nabver>
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/login' element={<Longin />} />
+    </Routes>
+    </>
+    
+      
   );
 }
 
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <div>
+           <h2>this is Home</h2>
+           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quisquam?</p>
+        <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
